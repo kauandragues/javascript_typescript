8b81a598e607b6296a1fbc81145f6364e2dd8c90
kauandragues <kaua.rodrigues1509@gmail.com>
2026-03-11 11:14:25 -0300

    aulas do dia 11/03/2026

---
+++ om/aula35/assets/js/main.js
@@ -0,0 +1,40 @@
+function main() {
+  const form = document.querySelector('form');
+  let resultSection = null;
+
+  function createResultSection() {
+    const body = document.querySelector('body');
+    const newSection = document.createElement('section');
+    newSection.setAttribute('class', 'container');
+
+    body.appendChild(newSection);
+    return newSection;
+  }
+
+  function updateResultSection(tag, tagContent) {
+    const newTag = document.createElement(tag);
+    newTag.innerHTML = tagContent;
+    resultSection.appendChild(newTag);
+  }
+
+  function checkInputs(tag, tagContent) {
+    if (tag == '' || tagContent == '') {
+      alert('Não deixe os campos vázios ou sem selecionar');
+      return false;
+    }
+    return true;
+  }
+
+  form.addEventListener('submit', evento => {
+    evento.preventDefault();
+    const tag = document.querySelector("select[name='tag']").value;
+    const tagContent = document.querySelector('textarea').value;
+    if (!checkInputs(tag, tagContent)) return;
+
+    if (resultSection == null) resultSection = createResultSection();
+
+    updateResultSection(tag, tagContent);
+  });
+}
+
+main();
